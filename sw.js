fd0b01d006ec3aeb66d65ec304a83b7f051cf2db
anwar-alfaqih <anwaralfaqih3@gmail.com>
2026-02-15 02:49:19 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'anwar-alfaqih-v2026';
+const ASSETS_TO_CACHE = [
+  'index.html',
+  'icon-512.png',
+  'icon-192.png',
+  'y4g.mp3',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700;900&display=swap'
+];
+
+// تثبيت الخدمة وتخزين الملفات الأساسية
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// استراتيجية "الشبكة أولاً مع العودة للتخزين المؤقت"
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// تنظيف النسخ القديمة من الكاش
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
